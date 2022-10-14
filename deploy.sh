aa86ac6a3ea7f8793cf21eaf3f16498171cdd97e
bdominic00@gmail.com <bdominic00@gmail.com>
2022-10-13 21:44:45 -0400

    changes

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@ USER=root
 HOST=192.168.1.50
 DIR=/var/www/html/   # the directory where your web site files should go
 
+git submodule init
+git submodule update
+
 hugo && rsync -avz --delete public/ ${USER}@${HOST}:${DIR}
 
 exit 0
